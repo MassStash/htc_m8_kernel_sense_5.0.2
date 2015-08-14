5f5d6fd508e3e2019aecedf2eb1fa22de099880b
massstash <massstash@gmail.com>
2015-08-13 20:30:22 -0500

    Fix 'cpufreq: Fix cores not restoring saved frequencies after replugging'
    Signed-off-by: massstash <massstash@gmail.com>

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 10 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#10 SMP PREEMPT Mon Jun 8 20:18:20 CDT 2015"
-#define LINUX_COMPILE_BY "MassStash"
-#define LINUX_COMPILE_HOST "zaichron-lin"
-#define LINUX_COMPILER "gcc version 4.9.3 20150311 (prerelease) (crosstool-NG 1.20.0 - Linaro GCC 2015.03 - Cortex-A15) "
