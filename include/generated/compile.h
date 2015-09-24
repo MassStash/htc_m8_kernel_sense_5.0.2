14d3fcea48696a0d9523f67ec5e5e332fe6949d6
MassStash <MassStash@gmail.com>
2015-09-24 11:01:04 -0500

    Remove generated stuff
    Signed-off-by: MassStash <MassStash@gmail.com>

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Tue Apr 14 18:28:55 CST 2015"
-#define LINUX_COMPILE_BY "yichen"
-#define LINUX_COMPILE_HOST "yichen-ThinkCentre-M90"
-#define LINUX_COMPILER "gcc version 4.7 (GCC) "
